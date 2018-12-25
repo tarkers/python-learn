908eee94130fa89bd45a4b7f5d4bb77b671b6706
tarkers <big.shrimp1117@gamil.com>
2018-12-25 10:59:19 +0800

    simple Grade system
    90 A
    80 B
    60 C
    0~60 D

---
+++ python/home.py
@@ -0,0 +1,15 @@
+grade =0
+while(grade!=-1):
+    grade=int(input('enter your grade(-1 to terminate):'))
+    if(grade>=90):
+        print('A')
+        continue
+    elif(grade>=90):
+        print('B')
+        continue
+    elif(grade>=60):
+        print('C')
+        continue
+    else:
+        print('D')
+print('the end')
